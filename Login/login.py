661aeb9c80fcac3519f778ba8f620fe1ba63cde8
Elena <elen.iva89@gmail.com>
2020-05-05 21:36:33 +0300

    Изменения

--- Login/login.py
+++ Login/login.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 import time
 
-host = "<stand>"
-login="<login>"
-password="<password>"
+#host = "beta.hi-tech.org"
+#login="admin@ivcs.su"
+#password="test"
 
 try:
     browser = webdriver.Chrome()
-    browser.get(host)
+    browser.get("beta.hi-tech.org")
     input1 = browser.find_element_by_xpath('//input[@type="text"]')
-    input1.send_keys(login)
+    input1.send_keys("admin@ivcs.su")
     input2 = browser.find_element_by_xpath("//input[@type='password']")
-    input2.send_keys(password)
+    input2.send_keys("test")
     #input1 = browser.find_element_by_xpath('//input[@type="text"]').clear()
 
     button = browser.find_element_by_xpath("//div[@class='ivcs-button-text']")
